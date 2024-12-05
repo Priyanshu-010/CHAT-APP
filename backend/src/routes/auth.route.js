9c2ba522d35a0415e5cfb917ac89c1224ae7383e
Priyanshu-010 <priyanshurai2772@gmail.com>
2024-12-05 12:35:22 +0530

    signup

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { signup, login, logout } from "../controllers/auth.controller.js";
 
-const router = express.Router();
+const authRoutes = express.Router();
 
-router.get("/signup", signup)
+router.post("/signup", signup)
 router.get("/login", login)
 router.get("/logout", logout)
 
