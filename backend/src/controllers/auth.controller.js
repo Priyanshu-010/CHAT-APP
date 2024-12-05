569c0b34f9fae122ec02dbf1048a1cf0df177147
Priyanshu-010 <priyanshurai2772@gmail.com>
2024-12-05 22:08:17 +0530

    Full auth backend Completed

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -83,3 +83,31 @@ export const logout = (req, res) => {
     res.status(500).json({ message: "Internal Server Error" })
   }
 }
+
+export const updateProfile = async (req, res) => {
+  try {
+    const {profilePic} = req.body;
+    const userId = req.user._id;
+
+    if(!profilePic) {
+      return res.status(400).json({ message: "Profile picture is required" });
+    }
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic)
+    const updatedUser = await User.findByIdAndUpdate(userId, {profilePic: uploadResponse.secure_url}, {new: true});
+
+    res.status(200).json({ message: "Profile picture updated successfully", profilePic: updatedUser.profilePic });  
+  } catch (error) {
+    console.log("Error in updateProfile controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+}
+
+export const checkAuth = (req, res) => {
+  try {
+    res.status(200).json(req.user);
+  } catch (error) {
+    console.log("Error in checkAuth controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+}
