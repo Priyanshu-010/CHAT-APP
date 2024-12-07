ba7fb7e709fad2eb69f4f5b23451f7aa8d00f581
Priyanshu-010 <priyanshurai2772@gmail.com>
2024-12-07 19:33:08 +0530

    profilePage

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -40,10 +40,10 @@ export const useAuthStore = create((set) => ({
     set({ isLoggingIn: true });
     try {
       const res = await axiosInstance.post("/auth/login", data);
+      console.log(res)
       set({ authUser: res.data });
       toast.success("Logged in successfully");
-
-      get().connectSocket();
+      // get().connectSocket();
     } catch (error) {
       toast.error(error.response.data.message);
     } finally {
@@ -55,9 +55,23 @@ export const useAuthStore = create((set) => ({
       await axiosInstance.post("/auth/logout");
       set({ authUser: null });
       toast.success("Logged out successfully");
-      get().disconnectSocket();
+      // get().disconnectSocket();
     } catch (error) {
       toast.error(error.response.data.message);
     }
   },
+  updateProfile: async (data) => {
+    set({ isUpdatingProfile: true });
+    try {
+      const res = await axiosInstance.put("/auth/update-profile", data);
+      set({ authUser: res.data });
+      toast.success("Profile updated successfully");
+    } catch (error) {
+      console.log("error in update profile:", error);
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isUpdatingProfile: false });
+    }
+  },
+
 }));
