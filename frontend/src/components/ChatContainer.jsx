7d140ccc080c371f4e4799d3499f61eaaf285834
Priyanshu-010 <priyanshurai2772@gmail.com>
2024-12-10 15:34:06 +0530

    Chat Header

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import { useChatStore } from "../store/useChatStore";
+import { useEffect, useRef } from "react";
+
+import ChatHeader from "./ChatHeader";
+import MessageInput from "./MessageInput";
+// import MessageSkeleton from "./skeletons/MessageSkeleton";
+import { useAuthStore } from "../store/useAuthStore";
+// import { formatMessageTime } from "../lib/utils";
 
 const ChatContainer = () => {
-  return (
-    <div>ChatContainer</div>
-  )
-}
+  const {
+    messages,
+    getMessages,
+    isMessagesLoading,
+    selectedUser,
+    // subscribeToMessages,
+    // unsubscribeFromMessages,
+  } = useChatStore();
+  const { authUser } = useAuthStore();
+  // const messageEndRef = useRef(null);
 
-export default ChatContainer
+  useEffect(() => {
+    getMessages(selectedUser._id);
+
+    // subscribeToMessages();
+
+    // return () => unsubscribeFromMessages();
+  }, [selectedUser._id, getMessages]);
+
+  useEffect(() => {
+    getMessages(selectedUser._id);
+  }, [selectedUser._id, getMessages]);
+
+  if (isMessagesLoading) {
+    return (
+      <div className="flex-1 flex flex-col overflow-auto">
+        <ChatHeader />
+        {/* <MessageSkeleton /> */}
+        <MessageInput />
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader />
+      <p>Messages ... </p>
+      <MessageInput />
+    </div>
+  );
+};
+export default ChatContainer;
